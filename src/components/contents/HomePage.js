b4ac08052846c811b9fff3dd5f3725ad132aa311
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-08-11 18:15:31 +1000

    Added a Homepage component for the app with some text

---
+++ src/components/contents/HomePage.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+
+const HomePage = (props) => {
+  return (
+    <div>
+      <h1>Welcome to ShowTech</h1>
+      <span>Your only stop for Techy Stuff.</span>
+    </div>
+  );
+};
+
+export default HomePage;
