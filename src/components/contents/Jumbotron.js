4bafff0b826dc9e37521978c0f6be478e1ba5dc1
Pashtunzoy <asifkhannoorzai@gmail.com>
2016-08-26 22:47:02 +1000

    Made some changes to jumbotron componenet and ran build

--- src/components/contents/Jumbotron.js
+++ src/components/contents/Jumbotron.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react'
+import React, { PropTypes } from 'react';
 
 const Jumbotron = (props) => {
   return (
@@ -6,17 +6,6 @@ const Jumbotron = (props) => {
       <section>
         <h1>Welcome to ShowTech</h1>
         <span>Your only stop for Techy Stuff.</span>
-        <article>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident,
-            sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-        </article>
       </section>
     </section>
   );
